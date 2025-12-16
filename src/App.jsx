2dbb93e77d745dc7482e7b585814bafa4fe9f5e6
aditya0kmr <69079096+aditya0kmr@users.noreply.github.com>
2025-12-17 05:04:34 +0530

    fix: Correct JSX syntax and formatting in App.jsx - fix geometry/material nesting

--- src/App.jsx
+++ src/App.jsx
@@ -12,8 +12,8 @@ function SimpleScene() {
       
       {/* Main Gear */}
       <mesh position={[0, 0, 0]} rotation={[0.4, 0.3, 0]}>
-<boxGeometry args={[4, 4, 4]} />        <meshStandardMaterial color="#ff69b4" metalness={0.8} roughness={0.2} />
-      </mesh>
+        <boxGeometry args={[4, 4, 4]} />
+                <meshStandardMaterial color="#ff69b4" metalness={0.8} roughness={0.2} />
       
       {/* Decorative spheres */}
       <mesh position={[-4, 2, 0]}>
